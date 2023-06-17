42ebedcd239bcc554edb71bb75b6bc8a31214671
Valentin <VPopa@talentingsoftware.com>
2023-06-17 23:40:40 +0300

    Add FakeStore microservice

---
+++ ConnectionHelperArtifact/FakeStore_Service/src/main/java/com/example/fakestore_service/FakeStoreResource.java
@@ -0,0 +1,29 @@
+package com.example.fakestore_service;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/store-api")
+public class FakeStoreResource {
+    private static final String API_URL = "https://fakestoreapi.com/products";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProducts() {
+        Client client = ClientBuilder.newClient();
+        Response response = client.target(API_URL)
+                .request(MediaType.APPLICATION_JSON)
+                .get();
+
+        if (response.getStatus() == Response.Status.OK.getStatusCode()) {
+            return response.readEntity(String.class);
+        } else {
+            return "Error sending GET request. Response code: " + response.getStatus();
+        }
+    }
+}
