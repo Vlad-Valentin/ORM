5b49bc1e3ec59dabbc216bbb8c081bca3b166d2b
Valentin <VPopa@talentingsoftware.com>
2023-06-19 09:29:15 +0300

    Code refactoring

--- EntityService/src/main/java/com/example/entity_service/ServerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.entity_service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class ServerApplication extends Application {
-
-}
--- LoginService/src/main/java/com/example/login_service/ServerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.login_service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class ServerApplication extends Application {
-
-}
